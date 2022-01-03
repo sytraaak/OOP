91d4020d97bd226a1199d929496078e5f6bf5cb8
sytraaak <burdajen@gmail.com>
2022-01-03 13:32:09 +0100

    Přidány soubory OOP

---
+++ OOP_Arena.py
@@ -0,0 +1,16 @@
+import random
+
+class Dice:
+    """
+    Nekonečně stěnná kostka
+    """
+    def roll(self, sides):
+        """
+        :param sides: int
+        :return: random integer between 1 and parameter
+        """
+        return random.randint(1,sides)
+
+help(int)
+dice = Dice()
+print(dice.roll(6))
